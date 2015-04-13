e859a816382cceb988a7c6938b9cc7142459fb17
nishank <t.nishank1990@gmail.com>
2015-04-13 16:33:05 +0530

    changes in __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
-    app.config.from_object(config["default"])
+    app.config.from_object(config[config_name])
     config[config_name].init_app(app)
     bootstrap.init_app(app)
     moment.init_app(app)
